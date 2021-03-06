f2e460bdf15777d26147327d33c50b1bc9ab368a
shivam <shivamdatta465@gmail.com>
2021-03-06 20:54:48 +0530

    first commit

---
+++ food/customer/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from customer.models import *
+# Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(Category)
+admin.site.register(OrderModel)
